d6a74a0b2c2555664bad95453e6635a3f04c29cf
luiz-kohler <luizfelipekohler03@gmail.com>
2025-09-03 15:52:08 -0300

    feat: some recursion algorithms

---
+++ Recursion/Program.cs
@@ -0,0 +1,29 @@
+﻿int CalculateFactorial(int number) => number != 1 ? number * CalculateFactorial(number-1) : 1;
+
+static int SumOfDigits(int number, int index = 0)
+{
+    var numSize = number.ToString().Length;
+
+    if (numSize == index)
+        return 0;
+
+    var arr = number.ToString()
+        .Select(x => int.Parse(x.ToString()))
+        .ToArray();
+
+    var numToSum = arr[index];
+
+    return numToSum + SumOfDigits(number, index+1);
+}
+
+ static int SumOfDigits_V2(int n)
+{
+    if (n < 10)
+        return n;
+
+    var n_rest_of_number_divided_by_10 = n % 10;
+    var n_divided_by_10 = n / 10;
+
+    return n_rest_of_number_divided_by_10 + SumOfDigits_V2(n_divided_by_10);
+}
+
